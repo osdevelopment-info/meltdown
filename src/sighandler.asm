370d8310bc62dc8f001ac24583b29983932ebef4
Uwe Plonus <u.plonus@gmail.com>
2018-01-16 21:39:11 +0100

    Added code to create a SIGSEGV signal

--- src/sighandler.asm
+++ src/sighandler.asm
@@ -1,10 +1,25 @@
 bits 64
 
+; struct sigaction {
+;        __sighandler_t sa_handler;
+;        unsigned long sa_flags;
+;        __sigrestore_t sa_restorer;
+;        sigset_t sa_mask;               /* mask last for extensibility */
+;};
+
+
 section .bss
-    scratch: resb 4096
+    scratch:      resb 4096
+    dest:         resb 1024*1024
+    src:          resb 1024*1024
 
 section .data
-    ssigsegv: db "Catched SIGSEGV"
+    ssigsegv:     db "Catched SIGSEGV"
+    slen_sigsegv: equ $-ssigsegv
+    sh:           dq sighandler  ; sa_handler
+                  dd 0x00000004  ; sa_flags (SA_SIGINFO)
+                  dq 0           ; sa_restorer
+                  dq 0           ; sa_mask
 
 section .text
     extern printqw
@@ -18,7 +33,26 @@ section .text
     global _start
 
 _start:
+    cld
+    mov   RCX,1
+    shl   RCX,21
+    mov   RSI,src
+    mov   RDI,dest
+copy:
+    movsb
+    loop  copy
 
     xor   RDI,RDI            ; exit code
+    mov   RAX,60             ; sys exit
+    syscall
+
+sighandler:
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,ssigsegv
+    mov   RDX,slen_sigsegv
+    syscall
+
+    mov   RDI,1              ; exit code
     mov   RAX,60             ; sys exit
     syscall
