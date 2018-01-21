a96b0f34c9ef5ccb20966b8cf8287f3437367c93
Uwe Plonus <u.plonus@gmail.com>
2018-01-21 16:26:35 +0100

    Moved detection code to subprogram

--- src/cachetiming.asm
+++ src/cachetiming.asm
@@ -22,77 +22,56 @@ section .text
     global _start
 
 _start:
-    mov   RBP,measures       ; load measure data
     mov   RSI,data           ; move address of data to RSI
-    add   RSI,4096
-
+    mov   RBP,measures
     clflush [RSI]            ; flush the cache
-    clflush [RSI+4096]       ; flush the cache
-    clflush [RSI+2048]       ; flush the cache
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
+    clflush [RSI+1024]       ; flush the cache
 
-    mov   RBX,[RSI]          ; read from memory
+    mov   RBX,[RSI]
 
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
+    call  time_calc
 
-    mov   RBX,[RSI]          ; read from memory
+    mov   RAX,R14
+    sub   RAX,R15
+    mov   [RBP],RAX
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
+    mov   RAX,R13
+    sub   RAX,R14
+    mov   [RBP+8],RAX
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
+    mov   RAX,[RBP]
+    sub   RAX,[RBP+8]
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
 
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
-
-    mov   RBX,[RSI+2048]     ; read from memory
-
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
-
-    mov   RBX,[RSI+2048]     ; read from memory
-
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
-
+    mov   RSI,data           ; move address of data to RSI
     clflush [RSI]            ; flush the cache
-    mfence                   ; make sure that all previous operations are executed
-    mov   RBX,[RSI]          ; read from memory
+    clflush [RSI+1024]       ; flush the cache
 
-    mfence                   ; make sure that all previous operations are executed
-    rdtsc                    ; read the time stamp counter
-    mfence                   ; make sure that all previous operations are executed
-    mov   [RBP],EAX
-    add   RBP,4
-    mov   [RBP],EDX
-    add   RBP,4
+    mov   RBX,[RSI+1024]
 
-    mov   RBP,measures       ; load measure data
-    mov   RAX,[RBP+8]
-    sub   RAX,[RBP]
-    add   RBP,8
+    call  time_calc
+
+    mov   RAX,R14
+    sub   RAX,R15
+    mov   [RBP],RAX
     mov   RDI,time
     call  printhqw
     mov   RAX,1              ; sys write
@@ -100,10 +79,9 @@ _start:
     mov   RSI,timing
     mov   RDX,len_timing
     syscall
-
-    mov   RAX,[RBP+8]
-    sub   RAX,[RBP]
-    add   RBP,8
+    mov   RAX,R13
+    sub   RAX,R14
+    mov   [RBP+8],RAX
     mov   RDI,time
     call  printhqw
     mov   RAX,1              ; sys write
@@ -111,32 +89,8 @@ _start:
     mov   RSI,timing
     mov   RDX,len_timing
     syscall
-
-    mov   RAX,[RBP+8]
-    sub   RAX,[RBP]
-    add   RBP,8
-    mov   RDI,time
-    call  printhqw
-    mov   RAX,1              ; sys write
-    mov   RDI,1              ; stdout
-    mov   RSI,timing
-    mov   RDX,len_timing
-    syscall
-
-    mov   RAX,[RBP+8]
-    sub   RAX,[RBP]
-    add   RBP,8
-    mov   RDI,time
-    call  printhqw
-    mov   RAX,1              ; sys write
-    mov   RDI,1              ; stdout
-    mov   RSI,timing
-    mov   RDX,len_timing
-    syscall
-
-    mov   RAX,[RBP+8]
-    sub   RAX,[RBP]
-    add   RBP,8
+    mov   RAX,[RBP]
+    sub   RAX,[RBP+8]
     mov   RDI,time
     call  printhqw
     mov   RAX,1              ; sys write
@@ -148,3 +102,40 @@ _start:
     xor   RDI,RDI            ; exit code
     mov   RAX,60             ; sys exit
     syscall
+
+; Get the time for reading data (with and w/o cache) by getting two values from
+; a probe array (with some distance)
+; - in
+; RSI: start of the probe array (must be at least 4100 bytes)
+; - out
+; R15: the 1st time stamp (start of 1st read)
+; R14: the 2nd time stamp (end of 1st read/start of 2nd read)
+; R13: the 3rd time stamp (end of 2nd read)
+; - clobbered
+; RAX
+; RBX
+; RDX
+time_calc:
+    lfence
+    rdtsc                    ; get the time stamp counter
+    shl   RDX,32             ; mov EDX to the high double word
+    add   RAX,RDX            ; add it to the low double word
+    mov   R15,RAX            ; save the value to R15
+
+    mov   RBX,[RSI]          ; load data from the probe array
+
+    lfence
+    rdtsc                    ; get the time stamp counter
+    shl   RDX,32             ; mov EDX to the high double word
+    add   RAX,RDX            ; add it to the low double word
+    mov   R14,RAX            ; save the value to R14
+
+    mov   RBX,[RSI+1024]     ; load data from the probe array
+
+    lfence
+    rdtsc                    ; get the time stamp counter
+    shl   RDX,32             ; mov EDX to the high double word
+    add   RAX,RDX            ; add it to the low double word
+    mov   R13,RAX            ; save the value to R13
+
+    ret
