426dbce5a28f6b159df542a81e5e4a155788369a
Uwe Plonus <u.plonus@gmail.com>
2018-01-21 14:36:35 +0100

    Added further code for testing caching timing

--- src/cachetiming.asm
+++ src/cachetiming.asm
@@ -1,13 +1,14 @@
 bits 64
 
 section .bss
-    data:   times 256 resb 4096
+    measures: resq 1024 
+    data:     times 256 resb 4096
 
 section .data
-    time1:  times 16 db 0
-            db 0x0a
-    time2:  times 16 db 0
-            db 0x0a
+    timing:     db "Timing: 0x"
+    time:       times 16 db "x"
+                db 0x0a
+    len_timing: equ $-timing
 
 section .text
     extern printqw
@@ -21,49 +22,127 @@ section .text
     global _start
 
 _start:
+    mov   RBP,measures       ; load measure data
     mov   RSI,data           ; move address of data to RSI
+    add   RSI,4096
 
     clflush [RSI]            ; flush the cache
+    clflush [RSI+4096]       ; flush the cache
+    clflush [RSI+2048]       ; flush the cache
+    mfence                   ; make sure that all previous operations are executed
     rdtsc                    ; read the time stamp counter
-    shl   RDX,32             ; move the value of EAX to the high double word
-    add   RAX,RDX            ; combine EDX:EAX to a single value
-    mov   R8,RAX             ; save the time stamp
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
 
     mov   RBX,[RSI]          ; read from memory
 
-    lfence                   ; make sure that all previous operations are executed
+    mfence                   ; make sure that all previous operations are executed
     rdtsc                    ; read the time stamp counter
-    shl   RDX,32             ; move the value of EAX to the high double word
-    add   RAX,RDX            ; combine EDX:EAX to a single value
-    mov   R9,RAX             ; save the time stamp
-    sub   RAX,R8             ; calculate the time used for the read
-    mov   R8,RAX
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
 
-    mov   RBX,[RSI]          ; read from (cached memory)
+    mov   RBX,[RSI]          ; read from memory
 
-    lfence                   ; make sure that all previous operations are executed
+    mfence                   ; make sure that all previous operations are executed
     rdtsc                    ; read the time stamp counter
-    shl   RDX,32             ; move the value of EAX to the high double word
-    add   RAX,RDX            ; combine EDX:EAX to a single value
-    sub   RAX,R9             ; calculate the time used for the read
-    mov   R9,RAX
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
 
-    mov   RDI,time1
-    mov   RAX,R8
+    mov   RBX,[RSI+2048]     ; read from memory
+
+    mfence                   ; make sure that all previous operations are executed
+    rdtsc                    ; read the time stamp counter
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
+
+    mov   RBX,[RSI+2048]     ; read from memory
+
+    mfence                   ; make sure that all previous operations are executed
+    rdtsc                    ; read the time stamp counter
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
+
+    clflush [RSI]            ; flush the cache
+    mfence                   ; make sure that all previous operations are executed
+    mov   RBX,[RSI]          ; read from memory
+
+    mfence                   ; make sure that all previous operations are executed
+    rdtsc                    ; read the time stamp counter
+    mfence                   ; make sure that all previous operations are executed
+    mov   [RBP],EAX
+    add   RBP,4
+    mov   [RBP],EDX
+    add   RBP,4
+
+    mov   RBP,measures       ; load measure data
+    mov   RAX,[RBP+8]
+    sub   RAX,[RBP]
+    add   RBP,8
+    mov   RDI,time
     call  printhqw
     mov   RAX,1              ; sys write
     mov   RDI,1              ; stdout
-    mov   RSI,time1
-    mov   RDX,17
+    mov   RSI,timing
+    mov   RDX,len_timing
     syscall
 
-    mov   RDI,time2
-    mov   RAX,R9
+    mov   RAX,[RBP+8]
+    sub   RAX,[RBP]
+    add   RBP,8
+    mov   RDI,time
     call  printhqw
     mov   RAX,1              ; sys write
     mov   RDI,1              ; stdout
-    mov   RSI,time2
-    mov   RDX,17
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
+
+    mov   RAX,[RBP+8]
+    sub   RAX,[RBP]
+    add   RBP,8
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
+
+    mov   RAX,[RBP+8]
+    sub   RAX,[RBP]
+    add   RBP,8
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
+    syscall
+
+    mov   RAX,[RBP+8]
+    sub   RAX,[RBP]
+    add   RBP,8
+    mov   RDI,time
+    call  printhqw
+    mov   RAX,1              ; sys write
+    mov   RDI,1              ; stdout
+    mov   RSI,timing
+    mov   RDX,len_timing
     syscall
 
     xor   RDI,RDI            ; exit code
