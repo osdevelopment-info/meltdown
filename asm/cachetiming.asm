1aa841975b77b08e2dbe422df7ae0f8711697877
Uwe Plonus <u.plonus@gmail.com>
2018-03-09 11:37:38 +0100

    Added first executable statement so that anything is seen during execution of the program

--- asm/cachetiming.asm
+++ asm/cachetiming.asm
@@ -1,14 +1,20 @@
 bits 64
-     global _start
+     global         _start
+     pagesize       equ  4096
+
+section .rodata
+     suncached:     db "Uncached Access Time: ",0x00
 
 section .bss
-     measures: resq 2048
-     padding:  resb 4096
+     measures:      resq 2048
+     padding:       resb 4096
      align 4096
-     data:     times 257 resb 4096
+     data:          times 257 resb 4096
 
 section .text
 _start:
+     mov       RDI,suncached
+     call      _print
      xor       RDI,RDI
      mov       RAX,60
      syscall
@@ -16,16 +22,17 @@ _start:
 _xorshift:
      mov       RCX,RSI
      shr       RCX,2
+     mov       EAX,EDX
 .next_random:
-     mov       EBX,EDX
-     shl       EDX,13
-     xor       EDX,EBX
-     mov       EBX,EDX
-     shr       EDX,17
-     xor       EDX,EBX
-     mov       EBX,EDX
-     shl       EDX,5
-     xor       EDX,EBX
+     mov       EBX,EAX
+     shl       EAX,13
+     xor       EAX,EBX
+     mov       EBX,EAX
+     shr       EAX,17
+     xor       EAX,EBX
+     mov       EBX,EAX
+     shl       EAX,5
+     xor       EAX,EBX
      stosd
      loop      .next_random
      ret
